7ac28efb0feac8b92b0e3ccb206f293da321de4c
Egee Boy Gutierrez <egeeboygutierrez91@gmail.com>
2014-02-27 11:07:02 +0800

    Modified git hub

--- public/index.php
+++ public/index.php
@@ -13,7 +13,7 @@
  * to the application root now.
  */
 chdir(dirname(__DIR__));
-//echo __DIR__;
+echo __DIR__;
 // Decline static file requests back to the PHP built-in webserver
 if (php_sapi_name() === 'cli-server' && is_file(__DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH))) {
     return false;

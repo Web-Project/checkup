ac5ce87b808a3048b56c6cb7e1d4bea4c65865b4
Egee Boy Gutierrez <egeeboygutierrez91@gmail.com>
2014-02-27 11:35:00 +0800

    Modified git hub

---
+++ config/autoload/database.local.php
@@ -0,0 +1,20 @@
+<?php
+
+return array(
+    'db' => array(
+        'driver'         => 'Pdo',
+        'dsn'            => 'mysql:dbname=zf2tutorial;host=localhost',
+    ),
+    'service_manager' => array(
+        'factories' => array(
+            'Zend\Db\Adapter\Adapter' => function ($serviceManager) {
+                $adapterFactory = new Zend\Db\Adapter\AdapterServiceFactory();
+                $adapter = $adapterFactory->createService($serviceManager);
+
+                \Zend\Db\TableGateway\Feature\GlobalAdapterFeature::setStaticAdapter($adapter);
+
+                return $adapter;
+            }
+        )
+    )
+);
